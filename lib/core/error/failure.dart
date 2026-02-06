8ff5f9970dee31d7a0bc86373ed14edb1cba97f4
choti <melkychoti@gmail.com>
2026-02-06 11:28:16 +0300

    Failure class

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+
+  const Failure(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class StorageFailure extends Failure {
+  const StorageFailure(super.message);
+}
+
+class ValidationFailure extends Failure {
+  const ValidationFailure(super.message);
+}
