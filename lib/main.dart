d408f2c0219078482c8c77c43f59add7f031cd25
choti <melkychoti@gmail.com>
2026-03-10 16:44:52 +0300

    Update main App with Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:land_measure/app.dart';
+import 'package:land_measure/features/measurement/data/repositories/local_plot_repository.dart';
+import 'package:land_measure/features/measurement/domain/usecases/save_plot_use_case.dart';
+import 'package:land_measure/features/measurement/presentation/bloc/measurement_bloc.dart';
+import 'package:land_measure/shared/services/location_service.dart';
 
 void main() {
-  runApp(const LandMeasureApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final locationService = LocationService();
+  final plotRepository = LocalPlotRepository();
+  final savePlotUseCase = SavePlotUseCase(plotRepository);
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => MeasurementBloc(
+            locationService: locationService,
+            savePlotUseCase: savePlotUseCase,
+          ),
+        ),
+      ],
+      child: const LandMeasureApp(),
+    ),
+  );
 }
